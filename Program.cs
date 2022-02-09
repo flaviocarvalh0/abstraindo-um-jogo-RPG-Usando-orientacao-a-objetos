35b456b8b27100da2efb5d062fc8a3c9c5c84e1c
flaviocarvalh0 <flavioalvespw@outlook.com>
2022-02-09 13:40:29 -0300

    Abstracao RPG

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace dotnet_poo
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            WriteLine("Hello World");
+        }
+    }
+}
